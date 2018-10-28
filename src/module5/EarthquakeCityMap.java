55181d9bcaa63aa153a593e229db7f28cea0ef5a
PiratBolot <zartemko@mail.ru>
2018-10-28 13:40:22 +0300

    Fixed a bug when you can click on the hidden marker

--- src/module5/EarthquakeCityMap.java
+++ src/module5/EarthquakeCityMap.java
@@ -153,7 +153,7 @@ public class EarthquakeCityMap extends PApplet {
 	private boolean selectMarkerIfHover(List<Marker> markers)
 	{
 		for (Marker marker : markers) {
-			if (marker.isInside(map, mouseX, mouseY)) {
+			if (marker.isInside(map, mouseX, mouseY) && !marker.isHidden()) {
 				lastSelected = (CommonMarker)marker;
 				lastSelected.setSelected(true);
 				return true;
